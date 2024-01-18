f29b45c43ef9d76fb4dbd6f7ac278f8b033d2bee
Mindpath <singh@12345akshay@gmail.com>
2024-01-18 14:18:17 +0530

    graphQL setup

---
+++ src/myComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_DATA = gql`
+  query {
+    
+  }
+`;
+
+const MyComponent = () => {
+  const { loading, error, data } = useQuery(GET_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  console.log(data);
+
+  return (
+    <div>
+      This is Div
+    </div>
+  );
+};
+
+export default MyComponent;
